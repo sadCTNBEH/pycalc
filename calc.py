9be1c6df6458ca1bc4c4f180703a61c09480f692
sadCTNBEH <stark3758@gmail.com>
2025-02-09 13:43:39 +0500

    commit

---
+++ calc.py
@@ -0,0 +1,22 @@
+def main(input: str):
+    try:
+        first = int(txt.split()[0])
+        second = int(txt.split()[2])
+        if txt.split()[3] != None:
+            raise Exception("throws Exception")
+        elif first < 0 or second < 0 or first > 10 or second > 10:
+            raise Exception("throws Exception")
+        else:
+            match txt.split()[1]:
+                case "+":
+                    print(first + second)
+                case "-":
+                    print(first - second)
+                case "*":
+                    print(first * second)
+                case "/":
+                    print(first // second)
+                case _:
+                    raise Exception("throws Exception")
+    except Exception as e:
+        print("throws Exception")
